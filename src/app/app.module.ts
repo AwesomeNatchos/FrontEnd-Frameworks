a6a73fb1ccbdb54ecff07687b0e58a53a7f1bb42
Natcha Uthaug <natcha.uthaug@gmail.com>
2023-05-02 20:03:24 +0200

    added nav bar and user-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserTableComponent } from './user-table/user-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-table/user-table.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.css']
+})
+export class UserTableComponent {
+
+  
+
+}
+
+
